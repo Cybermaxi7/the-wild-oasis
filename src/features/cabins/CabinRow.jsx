cbd010fbe6942d5db829a339a3ddc5fd0703b306
cybermaxi7 <maximusagbe@gmail.com>
2023-11-02 02:05:10 +0100

    implemented a reusable table

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -6,18 +6,9 @@ import { formatCurrency } from "../../utils/helpers";
 import CreateCabinForm from "./CreateCabinForm";
 import { useCreateCabin } from "./useCreateCabin";
 import { useDeleteCabin } from "./useDeleteCabin";
+import Table from "../../ui/Table";
 
-const TableRow = styled.div`
-    display: grid;
-    grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-    column-gap: 2.4rem;
-    align-items: center;
-    padding: 1.4rem 2.4rem;
 
-    &:not(:last-child) {
-        border-bottom: 1px solid var(--color-grey-100);
-    }
-`;
 
 const Img = styled.img`
     display: block;
@@ -56,7 +47,17 @@ export default function CabinRow({ cabin }) {
         regularPrice,
         description,
     } = cabin;
+//     const TableRow = styled.div`
+//     display: grid;
+//     grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+//     column-gap: 2.4rem;
+//     align-items: center;
+//     padding: 1.4rem 2.4rem;
 
+//     &:not(:last-child) {
+//         border-bottom: 1px solid var(--color-grey-100);
+//     }
+// `;
     function handleDuplicateCabin() {
         createCabin({
             name: `Copy of ${name}`,
@@ -69,8 +70,8 @@ export default function CabinRow({ cabin }) {
     }
     const { isDeleting, deleteCabin } = useDeleteCabin();
     return (
-        <>
-            <TableRow>
+        
+            <Table.Row>
                 <Img src={image} />
                 <Cabin>{name}</Cabin>
                 <div>Fits up to {maxCapacity} guests</div>
@@ -111,7 +112,7 @@ export default function CabinRow({ cabin }) {
                         </Modal.Window>
                     </Modal>
                 </div>
-            </TableRow>
-        </>
+            </Table.Row>
+      
     );
 }
